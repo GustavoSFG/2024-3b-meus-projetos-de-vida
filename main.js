211dcbcfab15478ab0d42c0506a86c8cbbe481c5
Gustavo da Silva Franchin - GUUUHYT <106678386+GustavoSFG@users.noreply.github.com>
2024-04-02 08:38:38 -0300

    02/03

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".abas-conteudo");
+const botoes = document.querySelectorAll('.botao');
+const textos = document.querySelectorAll('.abas-conteudo');
+
+const contadores = document.querySelectorAll('.contador');
+
+const tempoObjetivo01 = new Date('2024-11-30T00:00:00');
+const tempoObjetivo02 = new Date('2024-10-10T00:00:00');
+const tempoObjetivo03 = new Date('2024-12-25T00:00:00');
+const tempoObjetivo04 = new Date('2024-12-27T00:00:00');
+
+const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
+
+ for (let = i= 0; i<tempos.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+ }
+
 
 for(let i= 0; i<botoes.length; i++){
     botoes[i].onclick = function (){
@@ -12,4 +26,22 @@ for(let i= 0; i<botoes.length; i++){
     botoes[i].classList.add('ativo')
     textos[i].classList.add('ativo')
     }
+}
+
+
+function calculaTempo(tempoObjetivo) {
+let tempoAtual = new Date();
+let tempofinal = tempoObjetivo - tempoAtual
+
+let segundos = Math.floor(tempofinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos ";
 }
